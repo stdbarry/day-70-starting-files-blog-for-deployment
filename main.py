57489deaf222e62cc0baf922120f2347437640d2
stdbarry <harry.antwi@outlook.com>
2023-10-09 21:37:50 +0100

    Use env variable and already created Procfile.

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ This will install the packages from the requirements.txt for this project.
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-#app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')  --os not working, carry on for now
+# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')  #os not working, carry on for now
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -55,8 +55,8 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db") os not working carry on for now
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy()
 db.init_app(app)
 
